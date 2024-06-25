2d64206797d95ca8d83d611f4f58e1fc37141ad0
dhan-profile <dhanushkrish54@gmail.com>
2024-06-25 19:12:57 +0530

    25-06-2024 | 07:12PM

---
+++ src/main/java/webLayer/Validation.java
@@ -0,0 +1,16 @@
+package webLayer;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/Validation")
+public class Validation extends HttpServlet {
+//	request Object contains request received from client, response Object contains response to be sent to client 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+}
